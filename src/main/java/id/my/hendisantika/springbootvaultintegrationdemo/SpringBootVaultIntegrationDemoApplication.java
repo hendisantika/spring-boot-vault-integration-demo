9b62ea991e44c7ba90d224741b26532d10a28d76
hendisantika <hendisantika@yahoo.co.id>
2024-04-19 08:56:49 +0700

    Modify Main Class  🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springbootvaultintegrationdemo/SpringBootVaultIntegrationDemoApplication.java
+++ src/main/java/id/my/hendisantika/springbootvaultintegrationdemo/SpringBootVaultIntegrationDemoApplication.java
@@ -1,13 +1,32 @@
 package id.my.hendisantika.springbootvaultintegrationdemo;
 
+import id.my.hendisantika.springbootvaultintegrationdemo.config.ExternalApiConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-public class SpringBootVaultIntegrationDemoApplication {
+@EnableConfigurationProperties(ExternalApiConfig.class)
+public class SpringBootVaultIntegrationDemoApplication implements CommandLineRunner {
+    private static final Logger log = LoggerFactory.getLogger(SpringBootVaultIntegrationDemoApplication.class);
+
+    private final ExternalApiConfig externalApiConfig;
+
+    public SpringBootVaultIntegrationDemoApplication(ExternalApiConfig externalApiConfig) {
+        this.externalApiConfig = externalApiConfig;
+    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootVaultIntegrationDemoApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("External Api URI : {} ", externalApiConfig.getUri());
+        log.info("External Api Key : {} ", externalApiConfig.getApiKey());
+    }
 }
